001b2adc62f83848783654731a8b3675f9b17d5a
Kamanpreet Singh <kskaman@gmail.com>
2025-01-06 12:10:23 -0330

    added link section to home page and changed layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,32 +1,38 @@
 import Header from "./Header";
+import SocialLinks from "./SocialLinks";
 
 const Home = () => {
+  const introduction = `
+  Hi! I am an aspiring <strong>software developer</strong> passionate about exploring
+  the dynamic field of full-stack development. I am currently focused on building
+  responsive and user-friendly applications using technologies such as <strong>React,
+  TypeScript, Node.js, MongoDB, CSS,</strong> and <strong>Tailwind</strong>. While I am
+  currently more focused on frontend development, I am equally interested in backend
+  development. With a keen interest in solving real-world problems through innovative
+  digital solutions, I am committed to honing my skills and contributing meaningfully
+  to the tech industry.
+`;
+
   return (
     <>
       <Header />
-      <main className="max-w-3xl flex flex-col my-6 gap-12 justify-center py-20 px-6 sm:px-10 lg:px-14 text-zinc-600 dark:text-zinc-100">
-        <section>
+      <main
+        className="w-full my-4 lg:my-6 
+        grid grid-cols-1 lg:grid-cols-3 gap-12
+        py-8 lg:py-16 px-6 sm:px-10 lg:px-14
+        text-zinc-600 dark:text-zinc-100"
+      >
+        <section className="max-w-2xl lg:col-span-2">
           <h1 className="text-5xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
             I am Kaman, <br />
             Crafting My Journey in Software Development
           </h1>
-          <p className="mt-12">
-            Hi! I am an aspiring <strong>software developer</strong> passionate
-            about exploring the dynamic field of{" "}
-            <strong>full-stack development</strong>. I am currently focused on
-            building <strong>responsive</strong> and{" "}
-            <strong>user-friendly applications</strong> using technologies such
-            as <strong>React, TypeScript, Node.js, MongoDB, CSS,</strong> and{" "}
-            <strong>Tailwind</strong>. While I am currently more focused on{" "}
-            <strong>frontend development</strong>, I am equally interested in{" "}
-            <strong>backend development</strong>. With a keen interest in
-            solving <strong>real-world problems</strong> through{" "}
-            <strong>innovative digital</strong> solutions, I am committed to
-            honing my skills and contributing meaningfully to the{" "}
-            <strong>tech industry</strong>.
-          </p>
+          <p
+            className="mt-12"
+            dangerouslySetInnerHTML={{ __html: introduction }}
+          />
         </section>
-        <section>
+        <section className="max-w-2xl lg:col-span-2">
           <div className="flex flex-row gap-2.5 items-center">
             <span className="relative flex h-3 w-3">
               <span
@@ -35,8 +41,11 @@ const Home = () => {
               ></span>
               <span className="relative h-3 w-3 rounded-full bg-teal-500"></span>
             </span>
-            <p className="text-md">available for new opportunities</p>
+            <p className="text-base">available for new opportunities</p>
           </div>
+        </section>
+        <section className="lg:col-span-1 lg:col-start-3 lg:row-start-1">
+          <SocialLinks />
         </section>
       </main>
     </>
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoCheckmarkDoneSharp, IoCopyOutline } from "react-icons/io5";
+
+const SocialLinks: React.FC = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const email = "kskaman@gmail.com";
+
+  const handleCopyEmail = async () => {
+    try {
+      await navigator.clipboard.writeText(email);
+      setIsCopied(true);
+    } catch (err) {
+      console.error("Failed to copy email: ", err);
+    }
+  };
+
+  useEffect(() => {
+    if (isCopied) {
+      const timer = setTimeout(() => {
+        setIsCopied(false);
+      }, 4000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isCopied]);
+
+  return (
+    <>
+      <ul
+        className="flex flex-row items-center lg:items-start 
+             justify-start lg:flex-col gap-12 lg:gap-8"
+      >
+        <li className="lg:w-full">
+          <a
+            href="https://github.com/kskaman"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+            className="flex gap-2 hover:text-teal-500"
+          >
+            <FaGithub size={24} />
+            <span>GitHub</span>
+          </a>
+        </li>
+        <li className="lg:w-full">
+          <a
+            href="https://www.linkedin.com/in/kamanpreet-singh-b64672119/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+            className="flex gap-2 hover:text-teal-500"
+          >
+            <FaLinkedin size={24} />
+            <span>LinkedIn</span>
+          </a>
+        </li>
+        <li className="lg:w-full lg:pt-8 lg:border-solid lg:border-t-2">
+          <button
+            onClick={handleCopyEmail}
+            aria-label="Copy Email"
+            className="flex gap-2.5 items-center justify-center
+                       transition active:transition-none 
+                       bg-zinc-800 hover:bg-zinc-700 active:bg-zinc-800
+                       dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700
+                       font-semibold text-base text-zinc-100 active:text-zinc-100/70
+                       outline-offset-2 rounded-lg w-full sm:w-[148px] px-4 py-2.5"
+          >
+            {isCopied ? (
+              <IoCheckmarkDoneSharp size={24} />
+            ) : (
+              <IoCopyOutline size={24} />
+            )}
+            <span>{isCopied ? "Copied" : "Copy Email"}</span>
+          </button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default SocialLinks;
